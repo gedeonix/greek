c4a96806ae2d6c6acf6bd8180edaaf244468cc17
przemek <przemyslaw.soltan@gmail.com>
2020-09-01 17:35:11 +0200

    add simple transcript

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# greek
+# Gedeonix Greek Lib (transcript to pl)
+
+## Resource
+https://en.wikipedia.org/wiki/Greek_diacritics
+https://en.wikipedia.org/wiki/Latin_script_in_Unicode
---
+++ src/transcript.js
@@ -0,0 +1,67 @@
+
+const code = {
+
+    // duży alfabet
+    'Α': 'A',
+    'Β': 'B',
+    'Γ': 'G',
+    'Δ': 'D',
+    'Ε': 'E',
+    'Ζ': 'Dz',
+    'Η': 'E',
+    'Θ': 'Th',
+    'Ι': 'I',
+    'Κ': 'K',
+    'Λ': 'L',
+    'Μ': 'M',
+    'Ν': 'N',
+    'Ξ': 'Ks',
+    'Ο': 'O',
+    'Π': 'P',
+    'Ρ': 'R',
+    // luka
+    'Σ': 'S',
+    'Τ': 'T',
+    'Υ': 'Y',
+    'Φ': 'F',
+    'Χ': 'Ch',
+    'Ψ': 'Ps',
+    'Ω': 'O',
+
+    // mały alfabet
+    'α': 'a',
+    'β': 'b',
+    'γ': 'g',
+    'δ': 'd',
+    'ε': 'e',
+    'ζ': 'dz',
+    'η': 'e',
+    'θ': 'th',
+    'ι': 'i',
+    'κ': 'k',
+    'λ': 'l',
+    'μ': 'm',
+    'ν': 'n',
+    'ξ': 'ks',
+    'ο': 'o',
+    'π': 'p',
+    'ρ': 'r',
+    'ς': 's',
+    'σ': 's',
+    'τ': 't',
+    'υ': 'y',
+    'φ': 'f',
+    'χ': 'ch',
+    'ψ': 'ps',
+    'ω': 'o'
+}
+
+const re = new RegExp(Object.keys(code).join("|"),"gi");
+
+function transcript(str) {
+    return str.replace(re, (i, index) => {
+        // console.log(i)
+        return code[i] || i
+    })
+}
+module.exports = transcript
